c63a615cb026eb0b9ccb70682d1b760c9062840e
Matheusrrcs <79647787+Matheusrrcs@users.noreply.github.com>
2021-10-11 10:46:19 -0300

    Organização de linhas

--- HospDat/assets/js/rwdImageMaps.js
+++ HospDat/assets/js/rwdImageMaps.js
@@ -1,8 +1,3 @@
-
-$(document).ready(function (e) {
-    $('img[usemap]').rwdImageMaps();
-  
-    $('area').on('click', function () {
-      alert($(this).attr('alt') + ' clicked');
-    });
-  });
+$(document).ready(function(e) {
+  $('img[usemap]').rwdImageMaps();
+});
