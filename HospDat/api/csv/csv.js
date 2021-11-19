785c61e742409912c17c912d6a4acb194d1a8306
Matheus Rosario <matheus-santana-msn@hotmail.com>
2021-11-19 15:58:45 -0300

    a

--- HospDat/api/csv/csv.js
+++ HospDat/api/csv/csv.js
@@ -60,80 +60,6 @@ function estados(value) {
 function chart(object) {
 
 
-   var data = [
-      ['br-sp', 0],
-      ['br-ma', 1],
-      ['br-pa', 2],
-      ['br-sc', 5],
-      ['br-ba', 2],
-      ['br-ap', 5],
-      ['br-ms', 6],
-      ['br-mg', 7],
-      ['br-go', 8],
-      ['br-rs', 9],
-      ['br-to', 10],
-      ['br-pi', 11],
-      ['br-al', 12],
-      ['br-pb', 13],
-      ['br-ce', 14],
-      ['br-se', 15],
-      ['br-rr', 16],
-      ['br-pe', 17],
-      ['br-pr', 18],
-      ['br-es', 19],
-      ['br-rj', 20],
-      ['br-rn', 21],
-      ['br-am', 22],
-      ['br-mt', 23],
-      ['br-df', 24],
-      ['br-ac', 25],
-      ['br-ro', 26]
-   ];
-
-   // Create the chart
- 
-Highcharts.getJSON('https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/samples/data/germany.geo.json', function (geojson) {
-
-   // Initialize the chart
-   Highcharts.mapChart('container', {
-       chart: {
-           map: geojson
-       },
-
-       title: {
-           text: 'GeoJSON in Highmaps'
-       },
-
-       mapNavigation: {
-           enabled: true,
-           buttonOptions: {
-               verticalAlign: 'bottom'
-           }
-       },
-
-       colorAxis: {
-           tickPixelInterval: 100
-       },
-
-       series: [{
-           data: data,
-           keys: ['code_hasc', 'value'],
-           joinBy: 'code_hasc',
-           name: 'Random data',
-           states: {
-               hover: {
-                   color: '#a4edba'
-               }
-           },
-           dataLabels: {
-               enabled: true,
-               format: '{point.properties.postal}'
-           }
-       }]
-   });
-});
-             
-
 
 
 
@@ -141,3 +67,73 @@ Highcharts.getJSON('https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/sam
 }
 
  
+var data = [
+   ['br-sp', 0],
+   ['br-ma', 1],
+   ['br-pa', 2],
+   ['br-sc', 5],
+   ['br-ba', 2],
+   ['br-ap', 5],
+   ['br-ms', 6],
+   ['br-mg', 7],
+   ['br-go', 8],
+   ['br-rs', 9],
+   ['br-to', 10],
+   ['br-pi', 11],
+   ['br-al', 12],
+   ['br-pb', 13],
+   ['br-ce', 14],
+   ['br-se', 15],
+   ['br-rr', 16],
+   ['br-pe', 17],
+   ['br-pr', 18],
+   ['br-es', 19],
+   ['br-rj', 20],
+   ['br-rn', 21],
+   ['br-am', 22],
+   ['br-mt', 23],
+   ['br-df', 24],
+   ['br-ac', 25],
+   ['br-ro', 26]
+];
+
+// Create the chart
+Highcharts.mapChart('container', {
+   chart: {
+      map: 'countries/br/br-all'
+   },
+
+   title: {
+      text: 'Valor gasto por respiradores'
+   },
+
+   subtitle: {
+      text: 'Por estado do Brasil'
+   },
+
+   mapNavigation: {
+      enabled: true,
+      buttonOptions: {
+         verticalAlign: 'bottom'
+      }
+   },
+
+   colorAxis: {
+      min: 0
+   },
+
+   series: [{
+      data: data,
+      name: 'Valor gasto',
+      states: {
+         hover: {
+            color: '#e17468'
+         }
+      },
+      dataLabels: {
+         enabled: true,
+         format: '{point.name}'
+      }
+   }]
+});
+
