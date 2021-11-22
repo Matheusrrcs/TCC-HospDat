0a7cca81da393d0e8c9ad7fdc43b85515a7b31cd
Matheus Rosario <matheus-santana-msn@hotmail.com>
2021-11-21 23:46:18 -0300

    Update server.js

--- assets/api/server.js
+++ assets/api/server.js
@@ -1,14 +1,14 @@
-// const cors = require('cors')
+const cors = require('cors')
 const express = require('express')
 const app = express()
-// const CSVToJSON = require('csvtojson')
+const CSVToJSON = require('csvtojson')
 
-// CSVToJSON().fromFile("csv\\distribuicao_respiradores.csv").then(source => {
-//     Respiradores(source)
-// })
+CSVToJSON().fromFile("csv\\distribuicao_respiradores.csv").then(source => {
+    Respiradores(source)
+})
 
 
-// app.use(cors())
+app.use(cors())
 
     app.get('/titulos', (req, res) => {
         return res.json([
@@ -18,13 +18,13 @@ const app = express()
         ])
     })
 
-// function Respiradores(valor) {
+function Respiradores(valor) {
 
-//     app.get('/distribuicao_respiradores', (req, res) => {
-//         return res.json(valor)
-//     })
+    app.get('/distribuicao_respiradores', (req, res) => {
+        return res.json(valor)
+    })
 
-// }
+}
 
 // app.listen('4567')
 
