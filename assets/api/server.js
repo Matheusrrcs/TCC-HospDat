5b7bca5f8e67da7afe5079743832ba5629089cd5
Matheus Rosario <matheus-santana-msn@hotmail.com>
2021-11-22 21:41:07 -0300

    vamos lá ne

--- assets/api/server.js
+++ assets/api/server.js
@@ -1,36 +1,47 @@
-const cors = require('cors')
-const express = require('express')
-const app = express()
-const CSVToJSON = require('csvtojson')
+// const cors = require('cors')
+// const express = require('express')
+// const app = express()
+// const CSVToJSON = require('csvtojson')
 
-CSVToJSON().fromFile("csv\\distribuicao_respiradores.csv").then(source => {
-    Respiradores(source)
-})
-
-
-app.use(cors())
-
-    app.get('/titulos', (req, res) => {
-        return res.json([
-            { name: 'Distribuição de respiradores' }
+// CSVToJSON().fromFile("csv\\distribuicao_respiradores.csv").then(source => {
+//     Respiradores(source)
+// })
 
 
-        ])
-    })
+// app.use(cors())
 
-function Respiradores(valor) {
+//     app.get('/titulos', (req, res) => {
+//         return res.json([
+//             { name: 'Distribuição de respiradores' }
 
-    app.get('/distribuicao_respiradores', (req, res) => {
-        return res.json(valor)
-    })
+
+//         ])
+//     })
+
+// function Respiradores(valor) {
+
+//     app.get('/distribuicao_respiradores', (req, res) => {
+//         return res.json(valor)
+//     })
+
+// }
+
+// // app.listen('4567')
+
+
+// const port = process.env.PORT || 3000;
+
+// app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
+
+
+module.express = (req, res) => {
+
+    if (req.method === 'GET') {
+         res.json([
+             {name:'Rosario',idade:21}
+         ])
+    }else{
+
+    }
 
 }
-
-// app.listen('4567')
-
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
-
-
